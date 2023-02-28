f5ddaf0e31327fcfeeb2d92879c77b7eeb8bb7c5
zluuba <zluyba.nikitina@gmail.com>
2023-02-28 17:15:50 +0300

    Feat: add rect_area solution

---
+++ easy-level/rect_area.py
@@ -0,0 +1,12 @@
+# Calculate the area of a rectangle given its length and width
+#
+# Examples:
+# 0  == solution(0, 0)
+# 2  == solution(1, 2)
+# 9  == solution(3, 3)
+# 4096  == solution(64, 64)
+# 135  == solution(15, 9)
+
+
+def solution(width: int, length: int) -> int:
+    return width * length
