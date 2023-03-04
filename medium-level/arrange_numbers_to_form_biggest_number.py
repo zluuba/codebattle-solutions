4fe8e5bf1abfe2cb6d66e8be750e80890add8c77
zluuba <zluyba.nikitina@gmail.com>
2023-03-04 17:53:11 +0300

    Feat: add medium-level solution

---
+++ medium-level/arrange_numbers_to_form_biggest_number.py
@@ -0,0 +1,29 @@
+# Construct the largest possible number by arranging the integers from the given array.
+# Since the resulting number can be very large and out of int range, you have to represent it as string.
+# For example, from [3, 24, 4] we can construct 6 different numbers: 3244, 3424, 2434, 2443, 4324, 4243 and
+# the largest of them is 4324.
+
+# Examples:
+# "998764543431"  == solution([1,34,3,98,9,76,45,4])
+# "654321"  == solution([1,2,3,4,5,6])
+# "481428385220219710610"  == solution([481,428,385,202,2,197,106,10])
+# "6054854654"  == solution([54,546,548,60])
+
+
+from typing import List
+
+
+def solution(numbers: List[int]) -> str:
+    length = len(numbers)
+
+    if length == 1:
+        return str(numbers[0])
+
+    numbers = [str(num) for num in numbers]
+
+    for i in range(length):
+        for j in range(i + 1, length):
+            if numbers[i] + numbers[j] < numbers[j] + numbers[i]:
+                numbers[i], numbers[j] = numbers[j], numbers[i]
+
+    return ''.join(numbers)
