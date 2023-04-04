821e67c2d8d8ab2e950223d5be2df2f84e9555a3
zluuba <zluyba.nikitina@gmail.com>
2023-04-04 10:11:11 +0300

    fix: update hard-level problem solutions

--- hard-level/number_to_english.py
+++ hard-level/number_to_english.py
@@ -10,6 +10,36 @@
 
 
 def solution(num: int) -> str:
+    numbers = {
+        0: 'zero',
+        1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five',
+        6: 'six', 7: 'seven', 8: 'eight', 9: 'nine', 10: 'ten',
+        11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen',
+        15: 'fifteenth', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen',
+        19: 'nineteen', 20: 'twenty', 30: 'thirty', 40: 'forty',
+        50: 'fifty', 60: 'sixty', 70: 'seventy', 80: 'eighty',
+        90: 'ninety', 100: ' hundred ', 1000: ' thousand',
+    }
+
+    if num < 20:
+        return numbers[num].capitalize()
+    if num < 100:
+        if num % 10 == 0:
+            return numbers[num].capitalize()
+        else:
+            return (numbers[num - num % 10] + '-' + numbers[num % 10]).capitalize()
+    if num < 1000:
+        if num % 100 == 0:
+            return (numbers[num // 100] + numbers[100]).capitalize().strip()
+        else:
+            return (numbers[num // 100] + numbers[100] + solution(num % 100)).capitalize()
+    if num % 1000 == 0:
+        return (numbers[num // 1000] + numbers[1000] + ' ').capitalize().strip()
+    return (numbers[num // 1000] + numbers[1000] + ', ' + solution(num % 1000)).capitalize()
+
+
+# Alternative solution
+def solution2(num: int) -> str:
     ones = {
         0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four',
         5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine',
@@ -31,10 +61,8 @@ def solution(num: int) -> str:
             else:
                 res += tens[number // 10]
         else:
-            res += ones[number] + ' '
-        if number:
-            res += keyword
-        return res
+            res += ones[number] + " "
+        return res + keyword
 
     result = ""
     if 0 <= num <= 19:
